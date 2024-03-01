7257bc73b3944ae9c44aaa856b298354bb0cabe0
“sreelatha” <sreelatha.vallabhaneni@gmail.com>
2024-03-01 23:42:59 +0100

    created benfit component and updated benefits

--- src/components/benefits/index.tsx
+++ src/components/benefits/index.tsx
@@ -1,11 +1,40 @@
 import HText from "@/utils/HText";
-import { SelectedPage } from "@/utils/types";
+import { BenefitType, SelectedPage } from "@/utils/types";
 import {
     HomeModernIcon,
     UserGroupIcon,
     AcademicCapIcon,
   } from "@heroicons/react/24/solid";
   import { motion } from "framer-motion";
+import Benefit from "./Benefit";
+
+  const benefits: Array<BenefitType> = [
+    {
+      icon: <HomeModernIcon className="h-6 w-6" />,
+      title: "State of the Art Facilities",
+      description:
+        "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+    },
+    {
+      icon: <UserGroupIcon className="h-6 w-6" />,
+      title: "100's of Diverse Classes",
+      description:
+        "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+    },
+    {
+      icon: <AcademicCapIcon className="h-6 w-6" />,
+      title: "Expert and Pro Trainers",
+      description:
+        "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+    },
+  ];
+
+const container = {
+    hidden: {},
+    visible: {
+      transition: { staggerChildren: 0.2 },
+    },
+};
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -39,6 +68,24 @@ function Benefits({setSelectedPage}: Props) {
                     get you to your ultimate fitness goals with ease. We provide true
                     care into each and every member.
                 </p>
+            </motion.div>
+            {/* BENEFITS */}
+            <motion.div
+                className="mt-5 items-center justify-between gap-8 md:flex"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={container}
+            >
+                {benefits.map((benefit: BenefitType) => (
+                    <Benefit
+                        key={benefit.title}
+                        icon={benefit.icon}
+                        title={benefit.title}
+                        description={benefit.description}
+                        setSelectedPage={setSelectedPage}
+                    />
+                ))}
             </motion.div>
         </motion.div>
     </section>
